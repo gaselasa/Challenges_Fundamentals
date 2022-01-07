9221a687ecb14e574f129abf96118dc61568cc31
gaselasa <gaselasa@gmail.com>
2022-01-07 10:31:36 +0200

    add file fizzbuzz

---
+++ Fizzbuzz.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.mysort;
+
+/**
+ *
+ * @author User
+ */
+public class Fizzbuzz {
+    
+    
+    public static void main(String [] a){
+    for(int j=1;j<=100;j++)
+    {
+        
+        if(j%3==0) System.out.print("FIZZ ");
+        if(j%5==0)System.out.print("BUZZ ");
+        if(j%5==0&&j%3==0)System.out.print("FIZZBUZZ");
+
+            
+        
+        
+        
+    }   }
+    
+}
